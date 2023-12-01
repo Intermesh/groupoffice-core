616de1552d04bbaab1ffe5a1cebdc095dc3f8c69
Merijn Schering <mschering@intermesh.nl>
2023-12-01 14:37:13 +0100

    AddButton wrapper for extjs component

---
+++ script/components/AddButton.ts
@@ -0,0 +1,25 @@
+import {AutocompleteEventMap, Component, ComponentEventMap, Config, createComponent} from "@intermesh/goui";
+import {DetailPanel} from "./DetailPanel";
+import {UserDisplayCombo} from "./UserDisplayCombo";
+
+export class AddButton extends Component {
+	constructor() {
+		super();
+
+		this.on("added", (comp, index) => {
+			const panel = this.findAncestor(cmp => {
+				return cmp instanceof DetailPanel;
+			}) as DetailPanel;
+
+			// @ts-ignore
+			(this.items.get(0)! as any).detailView = panel.detailView;
+
+		})
+
+		this.items.add(new go.detail.addButton({
+			// detailView: this.detailView
+		}))
+	}
+}
+
+export const addbutton = (config?: Config<AddButton, ComponentEventMap<AddButton>>) => createComponent(new AddButton(), config);
