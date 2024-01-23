b22a615a4382b4cf58c65da9784ee90b0bc36ccc
Merijn Schering <mschering@intermesh.nl>
2024-01-23 10:22:57 +0100

    Some principal fixes

--- script/components/PrincipalCombo.ts
+++ script/components/PrincipalCombo.ts
@@ -6,7 +6,7 @@ import {Principal, User} from "../auth";
 
 export class PrincipalCombo extends ComboBox {
 
-	constructor(entity:string) {
+	constructor(entity?:string) {
 
 		super(jmapds<Principal>("Principal"));
 
@@ -17,13 +17,11 @@ export class PrincipalCombo extends ComboBox {
 			this.filter = {entity: 'User'};
 		}
 	}
-
-	pickerRecordToValue(_field: this, record: Principal): string {
-		return record.id.split(':').pop();
-	}
 }
 
-export const principalcombo = (config?: Omit<ComboBoxConfig<PrincipalCombo>, "dataSource">) => createComponent(new PrincipalCombo(), config);
+export type PrincipalComboConfig = Omit<ComboBoxConfig<PrincipalCombo>, "dataSource"> & {
+	entity?: string
+}
 
-// backwards-compat
-export const userdisplaycombo = (config?: Omit<ComboBoxConfig<PrincipalCombo>, "dataSource">) => createComponent(new PrincipalCombo('User'), config);
+export const principalcombo = (config?:PrincipalComboConfig) => createComponent(new PrincipalCombo(config?.entity), config);
+
