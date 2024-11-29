9a1c3a22577a53fbca0c45d74af43cfee158158f
Merijn Schering <mschering@intermesh.nl>
2024-11-29 16:25:05 +0100

    Filters without store

--- script/components/FilterPanel.ts
+++ script/components/FilterPanel.ts
@@ -1,8 +1,35 @@
-import {comp, Component, ComponentEventMap, Config, createComponent, FunctionUtil, Store} from "@intermesh/goui";
+import {
+	comp,
+	Component,
+	ComponentEventMap,
+	Config,
+	createComponent,
+	Filter,
+	FunctionUtil, Listener, ObservableListenerOpts,
+	Store
+} from "@intermesh/goui";
 
+
+/**
+ * @inheritDoc
+ */
+export interface FilterPanelEventMap<Type> extends ComponentEventMap<Type> {
+
+	filterchange: (button: Type, filter: Filter) => false | void
+
+
+	variablefilterchange: (button: Type, filter: Filter) => false | void,
+
+}
+
+export interface FilterPanel extends Component {
+	on<K extends keyof FilterPanelEventMap<this>, L extends Listener>(eventName: K, listener: Partial<FilterPanelEventMap<this>>[K], options?: ObservableListenerOpts): L
+	un<K extends keyof FilterPanelEventMap<this>>(eventName: K, listener: Partial<FilterPanelEventMap<this>>[K]): boolean
+	fire<K extends keyof FilterPanelEventMap<this>>(eventName: K, ...args: Parameters<FilterPanelEventMap<any>[K]>): boolean
+}
 export class FilterPanel extends Component {
 	private goFilterPanel: any;
-	constructor(public readonly entityName:string, public readonly store:Store) {
+	constructor(public readonly entityName:string, public readonly store?:Store) {
 		super();
 
 		this.items.add(
@@ -19,10 +46,18 @@ export class FilterPanel extends Component {
 				this.goFilterPanel.setWidth(this.el.offsetWidth);
 			}));
 
+			this.goFilterPanel.on("filterchange", (_p:any, filter:any) => {
+				this.fire("filterchange", this, filter);
+			})
+
+			this.goFilterPanel.on("variablefilterchange", (_p:any, filter:any) => {
+				this.fire("variablefilterchange", this, filter);
+			})
+
 			ro.observe(this.el);
 		});
 	}
 }
 
 
-export const filterpanel = (config: Config<FilterPanel, ComponentEventMap<FilterPanel>, "entityName"|"store">) => createComponent(new FilterPanel(config.entityName,config.store), config);
+export const filterpanel = (config: Config<FilterPanel, FilterPanelEventMap<FilterPanel>, "entityName"|"store">) => createComponent(new FilterPanel(config.entityName,config.store), config);
