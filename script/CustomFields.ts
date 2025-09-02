75ed69504232aed578501b39dc730b4aee9bbfa5
Merijn Schering <mschering@intermesh.nl>
2025-09-02 14:24:30 +0200

    Custom fields rendering in tables

--- script/CustomFields.ts
+++ script/CustomFields.ts
@@ -6,10 +6,9 @@ import {
 	Table,
 	TableColumn,
 	numbercolumn,
-	TableColumnConfig,
-	Format
+	textfield
 } from "@intermesh/goui";
-import {JmapDataSource} from "./jmap/index";
+import {JmapDataSource, jmapds} from "./jmap/index";
 import {AclItemEntity, AclOwnerEntity, groupDS, principalDS} from "./auth/index";
 
 
@@ -44,7 +43,7 @@ export interface Field extends AclItemEntity {
 export const fieldSetDS = new JmapDataSource<FieldSet>("FieldSet");
 export const fieldDS = new JmapDataSource<Field>("Field");
 
-type CustomFieldColumnCreator = (field:Field) => TableColumn;
+type CustomFieldColumnCreator = (field:Field) => AbstractCustomField;
 
 
 function findSelectOption (optionId:number, options:SelectOption[], path:string = ""): (SelectOption & {path:string})|undefined{
@@ -79,66 +78,66 @@ function findSelectOption (optionId:number, options:SelectOption[], path:string 
 	return undefined;
 }
 
-class CustomFields {
-	private fieldSets: Record<string, FieldSet[]> = {};
-	private fields: Record<string, Field[]> = {};
 
-
-	public async init() {
-		 const fs = await fieldSetDS.get();
-		 fs.list.forEach(fs => {
-			 if(!this.fieldSets[fs.entity])
-			 	this.fieldSets[fs.entity] = [];
-
-			 this.fieldSets[fs.entity].push(fs);
-		 })
-
-		const f = await fieldDS.get();
-		 f.list.forEach(f => {
-			 if(!this.fields[f.fieldSetId])
-				 this.fields[f.fieldSetId] = [];
-
-			 this.fields[f.fieldSetId].push(f);
-		 })
+abstract class AbstractCustomField {
+	constructor(protected readonly field: Field) {
 	}
 
-	getEntityFields(entity:string) {
-		const f:Field[] = [];
-
-		if(!this.fieldSets[entity]) {
-			return f;
-		}
-
-		this.fieldSets[entity].forEach(fs => {
-			if(this.fields[fs.id]) {
-				f.push(...this.fields[fs.id]);
-			}
+	public createFormField() {
+		return textfield({
+			name: this.field.name
 		})
-
-		return f;
 	}
 
-	private tableColumnCreators: Record<string, CustomFieldColumnCreator> = {
-		Date: f => datecolumn(this.getDefaultColumnConfig(f)),
-		DateTime: f => datetimecolumn(this.getDefaultColumnConfig(f)),
-		YesNo: f => boolcolumn(this.getDefaultColumnConfig(f)),
-		Checkbox: f => boolcolumn(this.getDefaultColumnConfig(f)),
-		Select: f => this.createSelectColumn(f),
-		User: f => this.createUserColumn(f),
-		Number: f => numbercolumn(Object.assign(this.getDefaultColumnConfig(f),  {decimals: f.options.decimals})),
-		FunctionField: f => numbercolumn(Object.assign(this.getDefaultColumnConfig(f),  {decimals: f.options.decimals})),
-		Group: f => this.createGroupColumn(f),
-		Html: f =>this.createDefaultColumn(f, {renderer: (v:string|undefined) => v ? v.stripTags() : ""}),
-		TextArea: f => this.createDefaultColumn(f, {renderer: (v:string|undefined)=> v ? v.replace(/\n/g, " ") : ""} )
+	public createTableColumn() {
+		return column(this.getColumnConfig());
 	}
 
-	private createSelectColumn(f:Field) {
+	protected getColumnConfig() {
+		return {
+			id: this.field.databaseName,
+			property: "customFields/" + this.field.databaseName,
+			header: this.field.name,
+			hidden: this.field.hiddenInGrid,
+			resizable: true
+		}
+	}
+}
+
+type ConstructableAbstractCustomField = {
+	new (...args: ConstructorParameters<typeof AbstractCustomField>): AbstractCustomField;
+};
+
+class TextCustomField extends AbstractCustomField {
+
+}
+
+class DateCustomField extends AbstractCustomField {
+	createTableColumn() {
+		return datecolumn(this.getColumnConfig());
+	}
+}
+
+class DateTimeCustomField extends AbstractCustomField {
+	createTableColumn() {
+		return datetimecolumn(this.getColumnConfig());
+	}
+}
+
+class YesNoCustomField extends AbstractCustomField {
+	createTableColumn() {
+		return boolcolumn(this.getColumnConfig());
+	}
+}
+
+class SelectCustomField extends AbstractCustomField {
+	createTableColumn() {
 		return column(
 			Object.assign(
-				this.getDefaultColumnConfig(f), {
+				this.getColumnConfig(), {
 					width: 100,
 					renderer:(columnValue: any, record: any, td: HTMLTableCellElement, table: Table, storeIndex: number, column: TableColumn)=> {
-						const o = findSelectOption(columnValue, f.dataType.options!);
+						const o = findSelectOption(columnValue, this.field.dataType.options!);
 						if(!o) {
 							return "";
 						}
@@ -155,53 +154,37 @@ class CustomFields {
 
 						return o.path;
 					}
-				})
+				}
+			)
 		)
 	}
+}
 
-
-	public registerTableColumnCreator(type:string, fn:CustomFieldColumnCreator) {
-		this.tableColumnCreators[type] = fn;
-	}
-
-	private getDefaultColumnConfig(f:Field) {
-		return {
-			id: f.databaseName,
-			property: "customFields/" + f.databaseName,
-			header: f.name,
-			hidden: f.hiddenInGrid,
-			resizable: true,
-			// renderer: (columnValue: any, record: any, td: HTMLTableCellElement, table: Table, storeIndex: number, column: TableColumn)=> {
-			// 	// TODO eliminate Extjs
-			// 	const type = go.customfields.CustomFields.getType(f.type);
-			// 	const cmp = new go.detail.Property();
-			// 	const v = ( type ? type.renderDetailView(columnValue, record, f) : columnValue);
-			// 	return v ? v +"" : "";
-			// }
-		}
-	}
-
-	private createDefaultColumn(f:Field, cfg:Partial<TableColumnConfig>) {
-		return column(Object.assign(this.getDefaultColumnConfig(f), cfg));
-	}
-
-	private fieldToTableColumn(f:Field) {
-		console.log(f.type);
-		return this.tableColumnCreators[f.type]
-			? this.tableColumnCreators[f.type](f)
-			: this.createDefaultColumn(f, {width: 100})
-	}
-
-	getTableColumns(entity:string) :TableColumn[] {
-		return this.getEntityFields(entity).map(f => {
-			return this.fieldToTableColumn(f);
-		})
-	}
-
-	private createUserColumn(f: Field) {
+class MultiSelectCustomField extends AbstractCustomField {
+	createTableColumn() {
 		return column(
 			Object.assign(
-				this.getDefaultColumnConfig(f), {
+				this.getColumnConfig(), {
+					width: 100,
+					renderer:(columnValue: any, record: any, td: HTMLTableCellElement, table: Table, storeIndex: number, column: TableColumn)=> {
+						if(!columnValue) {
+							return "";
+						}
+
+						return columnValue.map((id:number) => findSelectOption(id, this.field.dataType.options!)?.text).join(", ")
+
+					}
+				}
+			)
+		)
+	}
+}
+
+class UserCustomField extends AbstractCustomField {
+	createTableColumn() {
+		return column(
+			Object.assign(
+				this.getColumnConfig(), {
 					width: 100,
 					renderer: async (columnValue: any, record: any, td: HTMLTableCellElement, table: Table, storeIndex: number, column: TableColumn)=> {
 						if(!columnValue) {
@@ -213,11 +196,20 @@ class CustomFields {
 				})
 		)
 	}
+}
 
-	private createGroupColumn(f: Field) {
+class NumberCustomField extends AbstractCustomField {
+	createTableColumn() {
+		return numbercolumn(Object.assign(this.getColumnConfig(),  {decimals: this.field.options.decimals}))
+	}
+}
+
+
+class GroupCustomField extends AbstractCustomField {
+	createTableColumn() {
 		return column(
 			Object.assign(
-				this.getDefaultColumnConfig(f), {
+				this.getColumnConfig(), {
 					width: 100,
 					renderer: async (columnValue: any, record: any, td: HTMLTableCellElement, table: Table, storeIndex: number, column: TableColumn)=> {
 						if(!columnValue) {
@@ -226,10 +218,140 @@ class CustomFields {
 						const u = await groupDS.single(columnValue);
 						return u ? u.name : "";
 					}
-				})
+			})
 		)
+
+	}
+}
+
+class HtmlCustomField extends AbstractCustomField {
+	protected getColumnConfig(): { id: string; property: string; header: string; hidden: boolean; resizable: boolean } {
+		return Object.assign(super.getColumnConfig(), {renderer: (v:string|undefined) => v ? v.stripTags() : ""});
+	}
+}
+
+class TextAreaCustomField extends AbstractCustomField {
+	protected getColumnConfig(): { id: string; property: string; header: string; hidden: boolean; resizable: boolean } {
+		return Object.assign(super.getColumnConfig(), {renderer: (v:string|undefined)=> v ? v.replace(/\n/g, " ") : ""});
 	}
 }
 
 
+class ProjectCustomField extends AbstractCustomField {
+	createTableColumn() {
+		return column(
+			Object.assign(
+				this.getColumnConfig(), {
+					width: 100,
+					renderer: async (columnValue: any, record: any, td: HTMLTableCellElement, table: Table, storeIndex: number, column: TableColumn)=> {
+						if(!columnValue) {
+							return "";
+						}
+						const u = await jmapds("Project").single(columnValue);
+						return u ? u.name : "";
+					}
+				})
+		)
+
+	}
+}
+
+class ContactCustomField extends AbstractCustomField {
+	createTableColumn() {
+		return column(
+			Object.assign(
+				this.getColumnConfig(), {
+					width: 100,
+					renderer: async (columnValue: any, record: any, td: HTMLTableCellElement, table: Table, storeIndex: number, column: TableColumn)=> {
+						if(!columnValue) {
+							return "";
+						}
+						const u = await jmapds("Contact").single(columnValue);
+						return u ? u.name : "";
+					}
+				})
+		)
+
+	}
+}
+
+
+
+class CustomFields {
+	private fieldSets: Record<string, FieldSet[]> = {};
+	private fields: Record<string, Field[]> = {};
+
+
+	public async init() {
+		const fs = await fieldSetDS.get();
+		fs.list.forEach(fs => {
+			if (!this.fieldSets[fs.entity])
+				this.fieldSets[fs.entity] = [];
+
+			this.fieldSets[fs.entity].push(fs);
+		})
+
+		const f = await fieldDS.get();
+		f.list.forEach(f => {
+			if (!this.fields[f.fieldSetId])
+				this.fields[f.fieldSetId] = [];
+
+			this.fields[f.fieldSetId].push(f);
+		})
+	}
+
+	getEntityFields(entity: string) {
+		const f: Field[] = [];
+
+		if (!this.fieldSets[entity]) {
+			return f;
+		}
+
+		this.fieldSets[entity].forEach(fs => {
+			if (this.fields[fs.id]) {
+				f.push(...this.fields[fs.id]);
+			}
+		})
+
+		return f;
+	}
+
+	private types: Record<string, ConstructableAbstractCustomField> = {
+		Date: DateCustomField,
+		DateTime: DateTimeCustomField,
+		YesNo: YesNoCustomField,
+		Checkbox: YesNoCustomField,
+		Select: SelectCustomField,
+		MultiSelect: MultiSelectCustomField,
+		User: UserCustomField,
+		Number: NumberCustomField,
+		FunctionField: NumberCustomField,
+		Group: GroupCustomField,
+		Html: HtmlCustomField,
+		TextArea: TextAreaCustomField,
+		Text: TextCustomField,
+
+		// Todo, these should be added by the modules using registerTableColumnCreator
+		Project: ProjectCustomField,
+		Contact: ContactCustomField,
+	}
+
+
+	public registerType(type: string, cf: ConstructableAbstractCustomField) {
+		this.types[type] = cf;
+	}
+
+
+	private getType(f: Field) {
+		return this.types[f.type]
+			? new this.types[f.type](f)
+			: new this.types["Text"](f)
+	}
+
+	getTableColumns(entity: string): TableColumn[] {
+		return this.getEntityFields(entity).map(f => {
+			return this.getType(f).createTableColumn();
+		})
+	}
+}
 export const customFields = new CustomFields();
