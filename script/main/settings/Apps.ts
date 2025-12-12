edad7a423ffe1d8ef3a676622e219e8b2836991f
Merijn Schering <mschering@intermesh.nl>
2025-12-12 09:56:57 +0100

    collapsebtn target property

--- script/main/settings/Apps.ts
+++ script/main/settings/Apps.ts
@@ -1,10 +1,18 @@
-import {Component, t} from "@intermesh/goui";
+import {collapsebtn, comp, Component, h4, t, tbar} from "@intermesh/goui";
 import {AbstractSettingsPanel} from "./AbstractSettingsPanel.js";
 import {settingsPanels} from "./SettingsWindow.js";
 
 class Apps extends AbstractSettingsPanel {
 	constructor() {
 		super("apps", t("Apps"));
+
+
+		this.items.add(comp({
+
+		},
+			tbar({},
+				h4("Address book"), "->", collapsebtn({target: btn => btn.parent!.parent!})
+				)))
 	}
 }
 
