e626023469d45671705f8bf3834466a5dda84888
Merijn Schering <mschering@intermesh.nl>
2026-01-09 15:01:43 +0100

    Modules system settings

--- script/main/systemsettings/Apps.ts
+++ script/main/systemsettings/Apps.ts
@@ -1,19 +1,117 @@
-import {Panel, t} from "@intermesh/goui";
+import {
+	ArrayUtil,
+	btn,
+	checkbox,
+	comp,
+	Component,
+	ComponentEventMap,
+	h3,
+	h4,
+	img,
+	p,
+	panel,
+	Panel,
+	t,
+	tbar
+} from "@intermesh/goui";
 import {AbstractSystemSettingsPanel} from "./AbstractSystemSettingsPanel.js";
 import {systemSettingsPanels} from "./SystemSettingsWindow.js";
-import {User} from "../../auth/index.js";
-import {AppSettingsPanel} from "../settings/index.js";
+import {client} from "../../jmap/index.js";
+
+interface Module {
+	id: string
+	name: string
+	package: string
+	title: string
+	author: string
+	description: string
+	rights: any
+	status: string
+	packageTitle: string
+	enabled: boolean
+	installed: boolean
+	model: any
+	installable: boolean
+}
 
 class SystemSettingsApps extends AbstractSystemSettingsPanel {
+	private appContainer: Component;
 	constructor() {
 		super("apps", t("Apps"), "apps");
 
-		this.cls = "fit scroll";
 
-		this.items.add(...appSystemSettings.getPanels().map(p => new p))
+		// this.items.add(...appSystemSettings.getPanels().map(p => new p))
+
+		this.cls = "fit vbox";
+		this.items.add(
+			tbar({cls: "border-bottom"},
+				"->",
+				btn({
+					text: t("Install"),
+					icon: "add"
+				})),
+			this.appContainer = comp({cls: "apps", flex: 1},)
+			)
 	}
 
 	async load(): Promise<any> {
+
+		const response = await client.jmap("core/Module2/get")
+		console.log(response);
+
+		const sorted = ArrayUtil.multiSort(response.list, [{property: "packageTitle"}, {property: "title"}]) as Module[];
+
+		// sorted
+
+		let lastPackage = "";
+		sorted.filter(m => m.installed).forEach(m => {
+
+			if(lastPackage != m.packageTitle) {
+				this.appContainer.items.add(h3(m.packageTitle));
+				lastPackage = m.packageTitle;
+			}
+
+			this.appContainer.items.add(comp({
+				width: 400,
+				cls: "card module vbox",
+				itemId: m.id,
+				dataSet: {
+					icon: "apps"
+				}
+			},
+				img({src: client.downloadUrl("core/moduleImg/" + m.id)}),
+
+				h4(m.title),
+				p(m.description),
+				tbar({
+				},
+					btn({
+						cls: "filled",
+						hidden: m.installed,
+						text: t("Install"),
+					}),
+					checkbox({
+						type: "switch",
+						name: "enabled",
+						hidden: !m.installed,
+						value: m.enabled,
+						listeners:{
+							change: ({newValue}) => {
+								client.jmap("core/Module2/set", {
+									update: {[m.id]: {enabled: newValue}}
+								});
+							}
+						}
+					}),
+					"->",
+					btn({
+						text: t("Settings"),
+						hidden: !m.installed
+					})
+				)
+				))
+		})
+
 		return Promise.all(this.findChildrenByType(AppSystemSettingsPanel).map(p => p.load()));
 	}
 
@@ -41,14 +139,14 @@ export class AppSystemSettingsPanel extends Panel {
 
 
 class AppSystemSettings {
-	private panels: typeof AppSystemSettingsPanel[] = [];
+	private panels: Record<string, typeof AppSystemSettingsPanel[]> = {};
 
-	public addPanel(cmp: typeof AppSystemSettingsPanel) {
-		this.panels.push(cmp);
+	public addPanel(modulePackage:string, moduleName:string, cmp: typeof AppSystemSettingsPanel) {
+		this.panels[modulePackage+"/"+moduleName].push(cmp);
 	}
 
-	public getPanels() {
-		return this.panels;
+	public getPanels(modulePackage:string, moduleName:string) {
+		return this.panels[modulePackage+"/"+moduleName] ?? [];
 	}
 }
 
