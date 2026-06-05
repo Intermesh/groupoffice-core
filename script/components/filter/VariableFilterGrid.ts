f278b342232f232926e13fe2947e06ee56e9af02
Merijn Schering <mschering@intermesh.nl>
2026-06-05 11:40:28 +0200

    Check if entity is found

--- script/components/filter/VariableFilterGrid.ts
+++ script/components/filter/VariableFilterGrid.ts
@@ -136,5 +136,8 @@ export class VariableFilterGrid extends Table<Store, VariableFilterGridEventMap>
 		this.cls = "no-row-lines";
 
 		this.entity = entities.get(entityName);
+		if(!this.entity){
+			throw ` Entity ${entityName} not found!`;
+		}
 	}
 }
