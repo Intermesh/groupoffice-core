7d66fede22be24ce0f8228b13387531f4a05c81c
Merijn Schering <mschering@intermesh.nl>
2026-06-29 16:05:33 +0200

    Revert "createlinkbutton update"
    This reverts commit eec600e7a95c39b3226a0bfabc84b5d6ec2fe1ca.

--- script/components/CreateLinkButton.ts
+++ script/components/CreateLinkButton.ts
@@ -5,62 +5,38 @@
  */
 
 
-import {ButtonEventMap, Config, createComponent, DataSourceForm, OverlayToolbarButton, t, ToolbarItems} from "@intermesh/goui";
+import {ButtonEventMap, Config, createComponent, OverlayToolbarButton, t} from "@intermesh/goui";
 import {createlinkfield, CreateLinkField} from "./CreateLinkField";
-import {jmapds} from "../jmap";
 
 
 export class CreateLinkButton extends OverlayToolbarButton {
-	private _createLinkField?: CreateLinkField;
+	readonly createLinkField: CreateLinkField;
 
-	public get createLinkField() {
-		if(!this._createLinkField) {
-			this._createLinkField = createlinkfield({
-				style: {height: "100%"},
-				flex: 1,
-				listeners: {
-					setvalue: ({newValue}) => {
-						this.text = newValue && newValue.length ? newValue.length : "";
-					}
-				}
-			})
-
-			this.on("open", () => {
-				this._createLinkField!.focus();
-			})
-
-			const f = this.findAncestorByType(DataSourceForm);
-			if (f) {
-				f.on("save", () => {
-
-					this._createLinkField!.value.forEach((v: any) => {
-						void jmapds('Link').create({
-							fromId: f.currentId,
-							fromEntity: f.dataSource.id,
-							toId: v.entityId,
-							toEntity: v.entityName
-						})
-					});
-
-				}, {unshift: true});
-			}
-		}
-
-		return this._createLinkField;
-	}
-	protected getTbarItems(): ToolbarItems[] {
-
-		return [this.createLinkField];
-	}
 	constructor() {
 		super();
 
 		this.icon = "link";
+
 		this.title = t("Create links");
+
+		this.createLinkField = createlinkfield({
+			flex: 1,
+			listeners: {
+				setvalue: ({newValue}) => {
+					this.text = newValue && newValue.length ? newValue.length : "";
+				}
+			}
+		})
+
+		this.items.add(this.createLinkField)
+
+		this.on("open", () => {
+			this.createLinkField.focus();
+		})
 	}
 }
 
 /**
  * Shorthand function to create {@link CreateLinkButton}
- */
+*/
 export const createlinkbutton = (config?: Config<CreateLinkButton>) => createComponent(new CreateLinkButton(), config);
