f709c18c4a681da5e1cd46e01c3cf154491af686
Merijn Schering <mschering@intermesh.nl>
2026-08-21 12:25:55 +0200

    Fixed visible to panel

--- script/main/settings/VisibleTo.ts
+++ script/main/settings/VisibleTo.ts
@@ -1,29 +1,43 @@
 import {AbstractSettingsPanel} from "./AbstractSettingsPanel";
-import {t} from "@intermesh/goui";
+import {containerfield, datasourceform, t} from "@intermesh/goui";
 import {userSettingsPanels} from "./UserSettingsWindow.js";
 import {SharePanel, sharepanel} from "../../permissions";
 import {modules} from "../../Modules";
-import {User} from "../../auth";
+import {User, userDS} from "../../auth";
 
 userSettingsPanels.add(class VisibleTo extends AbstractSettingsPanel {
-	declare sharePanel: SharePanel
+	private sharePanel: SharePanel
 	constructor() {
 		super("visibleto", t("Visible to"), "visibility");
 		const rights = modules.get("core", "core")!.userRights;
 
 		this.items.add(
-				this.sharePanel = sharepanel({
-					name:'personalGroup.acl',
-					levels:[],
-					disabled: !rights.mayChangeUsers,
-				})
-
+			this.form = datasourceform({dataSource: userDS},
+				containerfield({
+					name: "personalGroup"
+				},
+					this.sharePanel = sharepanel({
+						name:'acl',
+						levels:[{value: "",name: ""},	{value: 10,name: t("Yes")}],
+						disabled: !rights.mayChangeUsers
+					})
+				)
+			)
 		);
-		//this.sharePanel.fitParent = true;
+
+		this.on("show", () => {
+			if(this.user) {
+				this.sharePanel.load();
+			}
+		})
 	}
 
 	async load(user: User): Promise<any> {
 		this.sharePanel.setEntity("Group", user.personalGroup.id);
+		if(!this.hidden) {
+			this.sharePanel.load();
+		}
+
 		return super.load(user);
 	}
 });
