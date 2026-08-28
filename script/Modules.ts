512ca2f915af4f132f6c261fc4f2beb726c35b70
Merijn Schering <mschering@intermesh.nl>
2026-08-28 11:33:43 +0200

    Remove old code and fixed missing link control in variable filters

--- script/Modules.ts
+++ script/Modules.ts
@@ -497,27 +497,27 @@ console.log(document.body.style.getPropertyValue("--fg-main"));
 			],
 
 
-			//
-			// customFieldTypes: [
-			// 	"go.customfields.type.Checkbox",
-			// 	"go.customfields.type.Date",
-			// 	"go.customfields.type.DateTime",
-			// 	"go.customfields.type.EncryptedText",
-			// 	"go.customfields.type.FunctionField",
-			// 	"go.customfields.type.Group",
-			// 	"go.customfields.type.Html",
-			// 	"go.customfields.type.MultiSelect",
-			// 	"go.customfields.type.Attachments",
-			// 	"go.customfields.type.Notes",
-			// 	"go.customfields.type.Number",
-			// 	"go.customfields.type.Select",
-			// 	"go.customfields.type.Text",
-			// 	"go.customfields.type.TextArea",
-			// 	"go.customfields.type.Data",
-			// 	"go.customfields.type.User",
-			// 	"go.customfields.type.YesNo",
-			// 	"go.customfields.type.TemplateField"
-			// ]
+// for legacy extjs3 modules
+			customFieldTypes: [
+				"go.customfields.type.Checkbox",
+				"go.customfields.type.Date",
+				"go.customfields.type.DateTime",
+				"go.customfields.type.EncryptedText",
+				"go.customfields.type.FunctionField",
+				"go.customfields.type.Group",
+				"go.customfields.type.Html",
+				"go.customfields.type.MultiSelect",
+				"go.customfields.type.Attachments",
+				"go.customfields.type.Notes",
+				"go.customfields.type.Number",
+				"go.customfields.type.Select",
+				"go.customfields.type.Text",
+				"go.customfields.type.TextArea",
+				"go.customfields.type.Data",
+				"go.customfields.type.User",
+				"go.customfields.type.YesNo",
+				"go.customfields.type.TemplateField"
+			]
 
 		});
 
